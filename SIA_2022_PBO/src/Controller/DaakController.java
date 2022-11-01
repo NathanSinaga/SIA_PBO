9e73f9b17ebc478ff114ba494a4edae1c5cd74d1
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2022-11-01 12:35:46 +0700

    Dominikus Yudistira menambahkan DAAKController yang berfungsi menyimpan fungsi-fungsi yang dapat di gunakan oleh DAAK

---
+++ SIA_2022_PBO/src/Controller/DaakController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Dosen;
+import Model.Mahasiswa;
+
+/**
+ *
+ * @author Nathan Sinaga
+ */
+public class DaakController {
+    
+    // Daak memasukkan Pengumuman
+    public void DaakMemasukkanPengumuman(String judul,String deskripsi){
+        
+        
+    }
+    // Daak memasukkan jadwal Mengajar
+    public void DaakMemasukkanJadwalMengajarDosen (Dosen dosen){
+        
+        
+    }
+    
+    // Daak memasukkan Jadwal Kuliah
+    public void DaakMemsukkanJadwalKuliah ( Mahasiswa mahasiswa){
+        
+    }
+    
+    // Daak Blokir absensi Mahasiswa
+    public void DaakBlokirAbsensiMahasiswa ( Mahasiswa mahasiswa){
+        
+    }
+}
