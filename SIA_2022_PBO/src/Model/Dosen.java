9e350a1e32f8b3970334ff78d6a11d39fc723f3d
Nathan Sinaga <Nathan Sinaga@LAPTOP-BBAGI8GT>
2022-11-20 16:00:33 +0700

    Update Tipe Data

--- SIA_2022_PBO/src/Model/Dosen.java
+++ SIA_2022_PBO/src/Model/Dosen.java
@@ -9,45 +9,63 @@ package Model;
  */
 public class Dosen extends Pengguna
 {
-    private int nikDosen;
-    private DosenKategori dosenKategori;
-    private int gajiDosen;
-
-    public Dosen(int nikDosen, DosenKategori dosenKategori, int gajiDosen, String nama, String password)
-    {
-        super(nama, password);
-        this.nikDosen = nikDosen;
-        this.dosenKategori = dosenKategori;
-        this.gajiDosen = gajiDosen;
+    private int id_jurusan;
+    private String nik_dosen;
+    private String nama_dosen;
+    private int gaji_dosen;
+    private String tipe_dosen;
+    
+    public Dosen(){
+        
+    }
+    
+    public Dosen(String email, String password, String tipe_pengguna, int id_jurusan, String nik_dosen, String nama_dosen, int gaji_dosen, String tipe_dosen ){
+        super(email, password, tipe_pengguna);
+        this.id_jurusan = id_jurusan;
+        this.nik_dosen = nik_dosen;
+        this.nama_dosen = nama_dosen;
+        this.gaji_dosen = gaji_dosen;
+        this.tipe_dosen = tipe_dosen;
+    }
+    
+    public void setId_Jurusan(int id_jurusan){
+        this.id_jurusan = id_jurusan;
+    }
+    
+    public void setNik_Dosen(String nik_dosen){
+        this.nik_dosen = nik_dosen;
+    }
+    
+    public void setNama_Dosen(String nama_dosen){
+        this.nama_dosen = nama_dosen;
+    }
+    
+    public void setGaji_Dosen(int gaji_dosen){
+        this.gaji_dosen = gaji_dosen;
+    }
+    
+    public void setTipe_Dosen(String tipe_dosen){
+        this.tipe_dosen = tipe_dosen;
+    }
+    
+    public int  getId_Jurusan(){
+        return this.id_jurusan;
+    }
+    
+    public String getNik_Dosen(){
+        return this.nik_dosen;
+    }
+    
+    public String getNama_Dosen(){
+        return this.nama_dosen;
+    }
+    
+    public int getGaji_Dosen(){
+        return this.gaji_dosen;
+    }
+    
+    public String getTipe_Dosen(){
+        return this.tipe_dosen;
     }
 
-    public int getNikDosen()
-    {
-        return nikDosen;
-    }
-
-    public void setNikDosen(int nikDosen)
-    {
-        this.nikDosen = nikDosen;
-    }
-
-    public DosenKategori getDosenKategori()
-    {
-        return dosenKategori;
-    }
-
-    public void setDosenKategori(DosenKategori dosenKategori)
-    {
-        this.dosenKategori = dosenKategori;
-    }
-
-    public int getGajiDosen()
-    {
-        return gajiDosen;
-    }
-
-    public void setGajiDosen(int gajiDosen)
-    {
-        this.gajiDosen = gajiDosen;
-    }
 }
