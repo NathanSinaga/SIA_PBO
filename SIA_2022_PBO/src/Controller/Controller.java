27150b9af8eb97b2cda1c154f43097d920a2ebbb
Nathan Sinaga <Nathan Sinaga@LAPTOP-BBAGI8GT>
2022-11-21 21:51:19 +0700

    Update Struktur Controller

--- SIA_2022_PBO/src/Controller/Controller.java
+++ SIA_2022_PBO/src/Controller/Controller.java
@@ -5,10 +5,37 @@
  */
 package Controller;
 
+import Model.Pengguna;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author Nathan Sinaga
  */
 public class Controller {
-    
+    static DatabaseHandler conn = new DatabaseHandler();
+
+    // SELECT ALL from table users
+    public static ArrayList<Pengguna> getAllUsers() {
+        ArrayList<Pengguna> users = new ArrayList<>();
+        conn.connect();
+        String query = "SELECT * FROM pengguna";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                Pengguna user = new Pengguna();
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setTipe_Pengguna(rs.getString("tipe_pengguna"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (users);
+    }
 }
