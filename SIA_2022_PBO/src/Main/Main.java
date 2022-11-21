94728548f25e13372616b1e3ade06060b129fc8c
Nathan Sinaga <Nathan Sinaga@LAPTOP-BBAGI8GT>
2022-11-21 18:34:50 +0700

    Update Struktur Controller

--- SIA_2022_PBO/src/Main/Main.java
+++ SIA_2022_PBO/src/Main/Main.java
@@ -17,6 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello world");
         // 1
         new FormLogin().setVisible(true);
         // 2
